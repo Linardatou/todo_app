b01e070fa2e59f99782e178747db96e49ff6e35a
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-01-09 13:16:12 +0200

    etsewe5tetwset5e5t

--- update.php
+++ update.php
@@ -1,3 +1,9 @@
 <?php
 //code to update the status from "In Progress" to "Complete!"
+if(array_key_exists('up', $_POST)){
+    update();
+  }
+  function update(){
+    $up = "UPDATE tasks SET status='Complete!' WHERE id";
+  }
 ?>
