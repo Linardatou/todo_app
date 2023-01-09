ecd35b1e64b0cb7d17f2a6f303e8a4b823be46b4
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-01-09 13:17:15 +0200

    prepei na grapseis giati den tha kanei to thing

--- delete.php
+++ delete.php
@@ -1,3 +1,12 @@
 <?php
 //button to delete data from table 
+
+
+if(array_key_exists('del', $_POST)){
+    diagrafei();
+  }
+  function diagrafei(){
+    $del = "DELETE FROM tasks WHERE id";
+    //$dl = mysqli_query($mysqli, $del);
+  }
 ?>
