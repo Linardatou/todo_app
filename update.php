f2e07ecd860205b91b735da292a34d0274c07fe6
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-01-16 13:05:40 +0200

    to jereis.php den mporei na kanei commit

--- update.php
+++ update.php
@@ -1,9 +1,29 @@
 <?php
-//code to update the status from "In Progress" to "Complete!"
-if(array_key_exists('up', $_POST)){
-    update();
+//button to update data from table 
+
+if(isset($_GET["id"])){
+    update_task();
+    header("Location: ./jereis.php");
   }
-  function update(){
-    //$up = " ";
+  function update_task(){
+
+    $hostname = "localhost";
+    $username = "root";
+    $password = ""; 
+    $dbname = "todoapp";
+    
+  //create connection
+  $mysqli = new mysqli($hostname, $username,  $password, $dbname);
+
+  $bob = ($row=["status"]==0);
+    
+  // Check connection
+  if ($mysqli->connect_error){
+    die("Connection failed: " . $mysqli->connect_error);
   }
-?> 
+    $up = "UPDATE tasks SET status='1' WHERE id=".$_GET["id"]; 
+    $mysqli->query($up);
+
+  }
+
+?>
