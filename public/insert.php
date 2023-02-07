6c3c93d2b3680b629dd3a39d689144ac375f767f
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-02-07 14:38:35 +0200

    MySQL prepared statements

--- public/insert.php
+++ public/insert.php
@@ -1,15 +1,28 @@
 
 <?php
 include "db.php";
-echo htmlspecialchars($_SERVER["PHP_SELF"]);
 
 if(isset($_POST['task'])) {
+    $title = filter_var($_POST['task'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $sql = "INSERT INTO tasks (`title`,`status`) VALUES ('$title', 0)";
     $cl = $mysqli->query($sql);
-    $title = filter_var($unfiltered_title, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     echo "Please put a valid task to do";
 }
 
 header("Location: index.php");
 
+//find way and how to sanitise and validate the input of user
+//$title is the object 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $filtered_title = filter_input(INPUT_POST,"task",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //$newtitle = filter_var($_POST['task'], FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    
+    function test_input($data){
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
 ?>
