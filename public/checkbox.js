f9ca1f052261e92f1d900ddbdf461fda2d1d7043
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-03-15 17:57:28 +0200

    agree with terms and service

--- public/checkbox.js
+++ public/checkbox.js
@@ -1,7 +1,5 @@
 var checkBox = document.getElementById("check");
 var text = document.getElementById("agree");
-
-
 function checks() {
     if (checkBox.checked == true){
       text.disabled = false;
