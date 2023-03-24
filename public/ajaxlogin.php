cd473a6145a240f7bb553de6a9ba36e6df5937e0
Katerina Linardatou <Katerina.Linardatou@mbww.com>
2023-03-24 18:09:12 +0200

    do message for when emails are the same

--- public/ajaxlogin.php
+++ public/ajaxlogin.php
@@ -23,6 +23,7 @@ $stmnt->execute([
     $username
 ]);
 $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+
 if(count($result)&& password_verify($password, $result[0]["password"])){
     $_SESSION["userid"]=$result[0]["id"];//this sets the session "userid that will allow the app to go the the index"
     exit(json_encode(["success"=> "ok"]));
--- public/ajaxsignup.php
+++ public/ajaxsignup.php
@@ -3,17 +3,11 @@ session_start();//start session
 include "db.php";//includes the code that is in db.php so it creates connection to the server
 $username = $password = $email = $fullname = "";
 
-// $number = preg_match('@[0-9]@', $password);//must have one character
-// $uppercase = preg_match('@[A-Z]@', $password);//must contain a capital letter
-// $lowercase = preg_match('@[a-z]@', $password);//must contain a lowercase letter
-//   $specialchars = preg_match('@[^\w]@', $password);//must have one special character
-
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['name'])){
     $username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
     $email = filter_var($_POST['email'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $fullname = filter_var($_POST['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $password = $_POST['password'];
-    //expression matches to establish requirements for password
     }
 
 if(empty($username)){
@@ -25,26 +19,32 @@ if(empty($username)){
 }else if(empty($password)){
     exit(json_encode(["success" => false, "msg" => "user password is empty."]));
 }
+$number = preg_match('@[0-9]@', $password);//must have one character
+$uppercase = preg_match('@[A-Z]@', $password);//must contain a capital letter
+$lowercase = preg_match('@[a-z]@', $password);//must contain a lowercase letter
+$sc = preg_match('@[^\w\?;]@', $password);//must have one special character
 
-//if(strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$specialchars){
-//    echo "<script>console.log(password needs atleast a number an uppercase and lowercase letter and a special character)</script>";
-//}else{
-    
+ if(strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$sc){
+     exit(json_encode(["success"=> false, "msg"=>"password needs at least a number, a lowercase and upercase letter and a special character"])); 
+ }else{ 
     $password_hash=password_hash($password,PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO users (`username`,`password`,`email`,`fullname`) VALUES (?,?,?,?)";
-    $pdo->prepare($sql)->execute([
+    try{
+        $sql = "INSERT INTO users (`username`,`password`,`email`,`fullname`) VALUES (?,?,?,?)";
+        $result=$pdo->prepare($sql)->execute([
         $username,
         $password_hash,
-        $email,
+        $email, 
         $fullname
     ]);
     $id = $pdo->lastInsertId();
     if($id){
         $_SESSION["userid"]=$id;//this sets the session "userid that will allow the app to go the the index"
         exit(json_encode(["success"=> "ok"]));
-        echo $_SESSION["password"];
     }else{
-        exit(json_encode(["success"=> "notok", "msg"=>"user cannot be saved"])); 
+        exit(json_encode(["success"=> "notok", "msg"=>print_r($result, true)])); 
     }
-//}
+    } catch(PDOException $e){
+        exit(json_encode(["success"=> "notok", "msg"=>$e->getMessage()])); 
+    }
+}
